bd3b763370a5103999549d7cb23c21268cbbcd8a
jobitsu <obitsu.jo@gmail.com>
2025-08-22 03:26:56 +0000

    add signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('', include('django.contrib.auth.urls'))
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
-# Create your views here.
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('/diary/')
+
+signup = SignUpView.as_view()
+
+"""
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.forms import UserCreationForm
+
+class SignUpView(View):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            return render(request, self.template_name, {'form': form})
+"""
